4fc44ee264028245cf4bb199f4d75a2c26ffd54c
Thomas <bourchisthomas@gmail.com>
2025-11-20 13:46:21 +0100

    correctoion signin

--- routes/users.js
+++ routes/users.js
@@ -47,7 +47,7 @@ router.post('/signup', (req, res) => {
 //--------------------------
 
 router.post('/signin', (req, res) => {
-  if (!checkBody(req.body, ['firstname','username', 'password'])) {
+  if (!checkBody(req.body, ['username', 'password'])) {
     res.json({ result: false, error: 'Missing or empty fields' });
     return;
   }
