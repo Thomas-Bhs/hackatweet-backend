d9d3aeee59e9ad20c9c3709aded82388debc438e
Thomas <bourchisthomas@gmail.com>
2025-11-20 12:00:54 +0100

    add connection

--- models/connection.js
+++ models/connection.js
@@ -3,4 +3,4 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.CONNECTION_STRING, {
   connectTimeoutMS: 2000})
   .then(() => console.log('✅ Connecté à MongoDB'))
-  .catch(error => console.error('❌ Erreur MongoDB:', error));
+  .catch(error => console.error('❌ Erreur MongoDB:', error));cd
